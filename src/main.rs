e60ea7f4a5d58f61e63e86a99dac22b9b7dfabea
vypxl <thomas@vypxl.io>
2023-02-16 15:54:41 +0100

    deduplicate short urls

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use actix_files::{NamedFile, Files};
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result};
+use actix_files::{Files, NamedFile};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Result};
 use chashmap::CHashMap;
 use handlebars::Handlebars;
-use rand::{thread_rng, Rng};
 use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
 use serde::Deserialize;
-use std::{collections::HashMap, sync::Mutex, sync::Arc, error::Error};
+use std::{collections::HashMap, error::Error, sync::Arc};
 
 #[derive(Deserialize)]
 struct FormData {
@@ -13,54 +13,73 @@ struct FormData {
 }
 
 struct AppState<'a> {
-  hb: Handlebars<'a>,
-  h: Arc<CHashMap<String, String>>
+    hb: Handlebars<'a>,
+    h: Arc<CHashMap<String, String>>,
+    rh: Arc<CHashMap<String, String>>,
 }
 
 #[get("/")]
 async fn index() -> Result<NamedFile, std::io::Error> {
-  NamedFile::open("static/index.html")
+    NamedFile::open("static/index.html")
 }
 
 #[get("/{target:[a-zA-Z0-9]{5}}")]
-async fn redirect<'a>(state: web::Data<AppState<'a>>, target: web::Path<String>) -> Result<web::Redirect> {
-  println!("Redirect");
-  let href = state.h.get(&target.into_inner()).ok_or(actix_web::error::ErrorNotFound(""))?;
-  Ok(web::Redirect::to(href.to_string()).temporary())
+async fn redirect<'a>(
+    state: web::Data<AppState<'a>>,
+    target: web::Path<String>,
+) -> Result<web::Redirect> {
+    let href = state
+        .h
+        .get(&target.into_inner())
+        .ok_or(actix_web::error::ErrorNotFound(""))?;
+    Ok(web::Redirect::to(href.to_string()).temporary())
 }
 
 #[post("/")]
-async fn post_index<'a>(state: web::Data<AppState<'a>>, data: web::Form<FormData>) -> Result<HttpResponse, Box<dyn Error>> {
-  let name = thread_rng()
-    .sample_iter(&Alphanumeric)
-    .take(5)
-    .map(char::from)
-    .collect::<String>();
-  state.h.insert(name.clone(), data.target.clone());
-  let mut yeet = HashMap::new();
-  yeet.insert("target", &name);
-  println!("{:?}", yeet);
-  let s = state.hb.render("posted", &yeet)?;
-  Ok(HttpResponse::Ok().body(s))
+async fn post_index<'a>(
+    state: web::Data<AppState<'a>>,
+    data: web::Form<FormData>,
+) -> Result<HttpResponse, Box<dyn Error>> {
+    let mut yeet = HashMap::new();
+    if let Some(name) = state.rh.get(&data.target) {
+        yeet.insert("target", name.to_string());
+    } else {
+        let name = thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(5)
+            .map(char::from)
+            .collect::<String>();
+        state.h.insert(name.clone(), data.target.clone());
+        state.rh.insert(data.target.clone(), name.clone());
+        yeet.insert("target", name);
+    }
+    let s = state.hb.render("posted", &yeet)?;
+    Ok(HttpResponse::Ok().body(s))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  let h = Arc::new(CHashMap::new());
-  HttpServer::new(move || {
-    let mut hb = Handlebars::new();
-    hb.register_templates_directory(".hbs", "templates").unwrap();
+    let h = Arc::new(CHashMap::new());
+    let rh = Arc::new(CHashMap::new());
+    HttpServer::new(move || {
+        let mut hb = Handlebars::new();
+        hb.register_templates_directory(".hbs", "templates")
+            .unwrap();
 
-    let data = AppState{hb, h: h.clone()};
+        let data = AppState {
+            hb,
+            h: h.clone(),
+            rh: rh.clone(),
+        };
 
-    App::new()
-      .app_data(web::Data::new(data))
-      .service(index)
-      .service(post_index)
-      .service(redirect)
-      .service(Files::new("/static", "static"))
-  })
-  .bind(("127.0.0.1", 8080))?
-  .run()
-  .await
+        App::new()
+            .app_data(web::Data::new(data))
+            .service(index)
+            .service(post_index)
+            .service(redirect)
+            .service(Files::new("/static", "static"))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
